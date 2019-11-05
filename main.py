e57d178576f1a8b13c8f70f0124018f3ad0b2f84
Natália <natalia.gama.mattos@hotmail.com>
2019-11-05 10:42:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 def metodo_minimos(x,y,tam):
-  for i in range (0, tam):                                          #somatória de xi
+  totalx = 0
+  totaly = 0
+  x_quadrado = 0
+  xy = 0
+  for i in range (0, tam):                                                   #somatória xi
     totalx += x[i]
-  for j in range(0, tam):                                           #somatória de y1
+  for j in range(0, tam):                                                    #somatória yi
     totaly += y[j]
-  for k in range (0, tam):                                          #somatória de xi^2
+  for k in range (0, tam):                                                   #somatória x^2
     x_quadrado += x[k]**2
-  for l in range(1, tam):                                           #somatória de xi*yi
+  for l in range(1, tam):                                                    #somaória x*y
     xy += x[l]*y[l]
-  a0 = (x_quadrado*totaly)-(xy*totalx)/(tam*x_quadrado)-(totalx**2) #calculo do a0
-  a1 = (tam*xy)-(totalx*totaly)/(tam*x_quadrado)-(totalx**2)        #calculo do a1
-  print("A reta aproximadora de pontos é {}*x+({})".format(a1,a0))
+  a0 = ((x_quadrado*totaly)-(xy*totalx))/((tam*x_quadrado)-(totalx**2))      #cálculo a0
+  a1 = ((tam*xy)-(totalx*totaly))/((tam*x_quadrado)-(totalx**2))             #cálculo a1
+  print("A reta aproximadora de pontos é {}*x+({})".format(a1,a0))  
 
 
-def main():                                                         #função principal
+def main():
   tam = int(input("Digite a quantidade de pontos: \n"))
-  x= [tam]
-  y=[tam]
+  c= [tam]
+  v=[tam]
   for j in range(0,tam):
-    x.append(j)
-    x[j] = float(input("Digite o valor de x: "))
+    c.append(j)
+    c[j] = float(input("Digite o valor de x: "))
   for i in range(0,tam):
-    y.append(i)
-    y[i]=float(input("\nDigite o valor de y: "))
-  metodo_minimos(x,y,tam)
+    v.append(i)
+    v[i]=float(input("\nDigite o valor de y: "))
+  metodo_minimos(c,v,tam)
 
 main()
